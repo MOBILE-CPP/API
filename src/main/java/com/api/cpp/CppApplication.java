044ea28169f3f3ee3ce5d32c86bfc2debcfa80ea
luis19nn <luis19nn@gmail.com>
2023-01-28 16:56:34 -0300

    first commit

---
+++ src/main/java/com/api/cpp/CppApplication.java
@@ -0,0 +1,13 @@
+package com.api.cpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CppApplication.class, args);
+	}
+
+}
